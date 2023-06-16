59ec510cae639b20e3bdbbe13e1fe0b9a55fa0f7
dystopianctzn <kinda.toxic11@gmail.com>
2023-06-16 18:48:37 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+Console.WriteLine("Размер папки {0} байт.", DirSize(new DirectoryInfo(@"C:\Riot Games"))); //путь
+
+
+static long DirSize(DirectoryInfo targetFolder)
+{
+    long size = 0;
+
+    try
+    {
+        FileInfo[] files = targetFolder.GetFiles();
+        foreach (FileInfo file in files)
+            size += file.Length;
+
+
+        DirectoryInfo[] subdirectories = targetFolder.GetDirectories();
+        foreach (DirectoryInfo sub in subdirectories)
+            size += DirSize(sub);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Произошла ошибка: " + ex.Message);
+    }
+
+    return size;
+}
